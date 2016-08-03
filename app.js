5e52805dbdeffc04d264d206c073962b6b001a71
Sherma-n <ysh.sherman@gmail.com>
2016-08-03 14:33:00 +0800

    inital commit

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
+* Sherman's Module Depencies
+*/
+const jwt = require ('jwt-simple');
+
+/**
  * Init Socket.io
  */
 const http = require('http');
@@ -68,7 +73,7 @@ app.use(sass({
 }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(session({
   resave: true,
